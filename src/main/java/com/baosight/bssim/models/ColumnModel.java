e2f8286c5ccdf2f63950de7e48cb20da23159be4
nswish <nswish@gmail.com>
2014-01-25 22:19:43 +0800

    ...

--- src/main/java/com/baosight/bssim/models/ColumnModel.java
+++ src/main/java/com/baosight/bssim/models/ColumnModel.java
@@ -46,24 +46,8 @@ public class ColumnModel {
         return new StringBuilder().append("\"").append(this.getUnderscoreCamelName()).append("\"").toString();
     }
 
-    public String getCapName(){
-        return StringUtils.capitalize(this.getCamelName());
-    }
-
-    public String getSetterName(){
-        return "set"+this.getCapName();
-    }
-
-    public String getGetterName() {
-        return "get"+this.getCapName();
-    }
-
     public String getComment() {
         return meta.get("comment")+"";
-    }
-
-    public String getQuoteComment() {
-        return new StringBuilder().append("\"").append(this.getComment()).append("\"").toString();
     }
 
     public String getType() {
@@ -112,76 +96,6 @@ public class ColumnModel {
 
     public int getScale() {
         return new BigDecimal(meta.get("scale")+"").intValue();
-    }
-
-
-/////////////////////////////////////////////// For Java Code Generation ///////////////////////////////////////////////
-    public String fragmentForAttr() {
-        return new StringBuilder().append("private " + getJavaType() + " " + getCamelName() + " = " + getDefaultValue() + ";").append("    /*" + getComment() + "*/").toString();
-    }
-
-    public String fragmentForGetter() {
-        return new StringBuilder().append("public " + getJavaType() + " " + getGetterName()).append("() {\n")
-                                  .append("    return this." + getCamelName()).append(";\n")
-                                  .append("}")
-                                  .toString();
-    }
-
-    public String fragmentForSetter() {
-        return new StringBuilder().append("public void " + getSetterName() + "(" + getJavaType() + " " + getCamelName()).append(") {\n")
-                                  .append("    if(this.isInDB && !taintedAttrs.containsKey(" + getQuoteCamelName() + "))taintedAttrs.put(" + getQuoteCamelName() + ", this." + getCamelName()).append(");\n")
-                                  .append("    this." + getCamelName() + " = " + getCamelName()).append(";\n")
-                                  .append("}")
-                                  .toString();
-    }
-
-    public String fragmentForMeta() {
-        StringBuilder result = new StringBuilder();
-
-        result.append("eiColumn = new EiColumn(" + getQuoteCamelName() + ");\n")
-              .append("eiColumn.setDescName(" + getQuoteComment() + ");\n")
-              .append("eiColumn.setFieldLength(" + getLength() + ");\n");
-
-        if ("N".equals(getType())){
-            result.append("eiColumn.setType(\"N\");\n")
-                  .append("eiColumn.setScaleLength(" + getScale() + ");\n");
-        }
-
-        return result.append("eiMetadata.addMeta(eiColumn);").toString();
-    }
-
-    public String fragmentForFromMap() {
-        if ("C".equals(getType())) {
-            return getSetterName() + "(StringUtils.defaultIfEmpty(StringUtils.toString(map.get(" + getQuoteCamelName() + ")), " + getCamelName() + "));";
-        } else if("N".equals(getType())) {
-            return getSetterName() + "(NumberUtils.to" + getJavaType() + "(StringUtils.toString(map.get(" + getQuoteCamelName() + ")), " + getCamelName() + "));";
-        } else {
-            return "";
-        }
-    }
-
-    public String fragmentForToMap() {
-        return "map.put(" + getQuoteCamelName() + ", StringUtils.toString(" + getCamelName() + ", eiMetadata.getMeta(" + getQuoteCamelName() + ")));";
-    }
-
-    public String fragmentForField() {
-        String comment = CodeHelper.formatComment(this.getComment());
-        String content = "public static final String " + this.getCamelName() + " = " + this.getQuoteCamelName() + ";";
-        return comment + "\n" + content;
-    }
-
-    public String fragmentForColumn() {
-        String comment = CodeHelper.formatComment(this.getComment());
-        String content = "public static final String " + getName() + " = " + getQuoteName() + ";";
-        return comment + "\n" + content;
-    }
-
-    public String fragmentForFromAttrs() {
-        return "if(attrs.get(" + getQuoteCamelName() + ") != null)" + getSetterName() + "((" + getJavaType() + ")attrs.get(" + getQuoteCamelName() + "));";
-    }
-
-    public String fragmentForToAttrs() {
-        return "result.put(" + getQuoteCamelName() + ", this." + getCamelName() + ");";
     }
 
 /////////////////////////////////////////////// For Xml Code Generation ////////////////////////////////////////////////
